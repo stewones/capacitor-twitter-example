27ca96c4a79edb8c3bff85b9ccba61afc65f5199
Stewan <oi@stewan.io>
2019-03-22 00:34:44 -0300

    fix(android)

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -21,15 +22,6 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //
-        // initialize twitter
-//        TwitterConfig config = new TwitterConfig.Builder(this)
-//                .logger(new DefaultLogger(Log.DEBUG))
-//                .twitterAuthConfig(new TwitterAuthConfig("FhMx9qRyTMv0L0OFfr7Wr6V0F", "4Pff72bcek2EWbpUP4eaWvrFvgcSdXrjq3ZuLkqCN4TQXlOFGk"))
-//                .debug(true)
-//                .build();
-//        Twitter.initialize(config);
-
         // Initializes the Bridge
         this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
             // Additional plugins you've installed go here
@@ -43,15 +35,19 @@ public class MainActivity extends BridgeActivity {
 
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // Pass the activity result to the auth client.
-        Log.d("DEBUG MAIN", String.valueOf(requestCode));
-
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // authClient.onActivityResult(requestCode, resultCode, data);
-
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        // Pass the activity result to the auth client.
+//        Log.d("DEBUG MAIN", String.valueOf(requestCode));
+//
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//
+//
+//
+//
+//        // authClient.onActivityResult(requestCode, resultCode, data);
+//
+//    }
 }
